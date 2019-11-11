302a76e793747a59a79621d924b3c30af1bcc7a3
Ubuntu <ubuntu@ip-172-31-22-149.ec2.internal>
2019-11-11 19:28:04 +0000

    commit message

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+
+# Created by: Manuel
+# Created on: November 2019
+# this is "The Hello World" program on the PyBadge
+
+
+def main():
+    # this function prints to the screen and console
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World")
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass  # jsut a place holder
+
+
+if __name__ == "__main__":
+    main()
